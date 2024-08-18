5bc22271109467c301b4e85ba0b251fe7120f4a2
Timothy Kim <ttk2@GS23491.SP.CS.CMU.EDU>
2024-08-17 20:45:21 -0700

    Add back

---
+++ scripts/resources/start_hdfs_cluster.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# define absolute path
+workspace_dir="/proj/HeARTy/ceridwen-sosp-2024-artifact"
+
+# move to hdfs scripts dir
+cd "${workspace_dir}/hdfs/scripts"
+
+# build hdfs from source
+bash install-hdfs.sh
+
+# copy configs to dist configs
+cp configs/* "${workspace_dir}/hdfs/hadoop-dist/target/hadoop-3.3.1/etc/hadoop/"
+
+# start
+yes Y | bash start-dfs.sh
+
+# add policies
+bash add-policies.sh
