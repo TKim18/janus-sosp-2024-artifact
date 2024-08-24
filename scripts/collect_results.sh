c6acf085e46356e430fb2668f1377ae841557213
Timothy Taeho Kim <ttk2@andrew.cmu.edu>
2024-08-24 11:17:01 -0400

    Fix seekwatcher

--- scripts/collect_results.sh
+++ scripts/collect_results.sh
@@ -6,7 +6,7 @@ WORKLOAD=$1
 
 # aggregate blktrace output
 cd ${RESULTS_DIR}/"$WORKLOAD"/output
-seekwatcher -t ${RESULTS_DIR}/"$WORKLOAD"/blktrace_raw/h
+sudo /proj/sosp24eval/janus-sosp-2024-artifact/scripts/seekwatcher -t ${RESULTS_DIR}/"$WORKLOAD"/blktrace_raw/h
 
 # aggregate space results
 ${SCRIPTS_DIR}/aggregate_space.py "$WORKLOAD"
